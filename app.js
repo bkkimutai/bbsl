988883011a60ceec28f2f223b312a260b4037b72
Simon Muiruri Njuguna <SMuiruri1@safaricom.co.ke>
2023-05-04 12:49:57 +0300

    middleware added

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 
 const aboutusRouter = require('./routes/aboutus');
+const servicesRouter = require('./routes/services');
 
 app.set('view engine', 'ejs');
 
 // add middleware
 app.use(aboutusRouter.router);
+app.use(servicesRouter.router);
 
 
 app.listen(3000, ()=>{
