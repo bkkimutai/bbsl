7c82a82511bf78d581df794d4487cc9f4fcc1e04
Lewis Ng’ate <lewisngate@Lewiss-MacBook-Air.local>
2023-05-11 01:59:28 +0300

    initial commit

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const aboutusRouter = require('./routes/aboutus');
 const servicesRouter = require('./routes/services');
 const loginRouter = require('./routes/login');
 const registerRouter = require('./routes/register');
+const productsRouter = require('./routes/products');
 
 app.set('view engine', 'ejs');
 
@@ -14,6 +15,7 @@ app.use(servicesRouter.router);
 app.use(loginRouter.router);
 app.use(registerRouter.router);
 app.use(express.static('public'));
+app.use(productsRouter.router);
 
 
 
