5259a769de8299792b80cb92ea1a1d61efc2f3fb
Bernard Amaitsa <besimani@gmail.com>
2023-05-23 14:32:12 +0300

    update modified files

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const express = require ('express');
-const app = express();
+const homeRouter = require('./routes/home');
+
+const app = express(); 
 
 app.set('view engine', 'ejs');
+
+app.use(homeRouter.router);
+app.use(express.static('public'));
+app.listen(5502, ()=>{
+    console.log("Application is starting...")
+});
---
+++ routes/home.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get("./home", (req, res)=>{
+    
+    res.render("home", {});
+})
+
+router.post("/home", (req, res)=>{
+    console.log("home page accessed - POST");
+});
+
+exports.router = router;
